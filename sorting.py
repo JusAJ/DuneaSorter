eb94bf940b3c8b929550a71f31eda1ad3ce2aabf
Arjen van Heteren <justaj@live.nl>
2025-08-12 12:55:53 +0000

    Added processing of Email. Solved some magic strings

--- sorting.py
+++ sorting.py
@@ -2,20 +2,26 @@ import csv
 import os
 import re
 
+VoiceName = 'voice'
+EmailName = 'e-mail'
+PhoneNumberColumn = 'Remote'
+EmailColumn = 'From'
+UsersColumn = 'Users'
+
 def newHeader(row):
     parsedHeader = dict()
 
-    if row['Media Type'] == 'voice':
+    if row['Media Type'] == VoiceName:
         parsedHeader['Soort contact'] = 'telefonie'
-    elif row['Media Type'] == 'e-mail':
+    elif row['Media Type'] == EmailName:
         parsedHeader['Soort contact'] = 'email'
     else:
         print('Een mediatype is gevonden dat niet ondersteund wordt. Het mediatype is: ' + row['Media Type'])
         raise ValueError()
 
-    parsedHeader['Gebruikers'] = row['Users']
-    parsedHeader['Telefoonnummer'] = row['Remote']
-    parsedHeader['Email'] = row['From']
+    parsedHeader['Gebruikers'] = row[UsersColumn]
+    parsedHeader['Telefoonnummer'] = row[PhoneNumberColumn]
+    parsedHeader['Email'] = row[EmailColumn]
     parsedHeader['Aan'] = row['To']
     parsedHeader['Datum'] = row['Date']
     parsedHeader['Duur'] = row['Duration']
@@ -26,6 +32,7 @@ def newHeader(row):
 
     return parsedHeader
 
+
 def removeMultipleNames(file):
     newFile = []
 
@@ -35,14 +42,21 @@ def removeMultipleNames(file):
     #| $       End of string
 
     for row in file:
-        nameLine = row['Users']
+        nameLine = row[UsersColumn]
         match = re.search(r'[^;]*\s*$', nameLine)
         if match:
-            row['Users'] = match.group(0).strip()
+            row[UsersColumn] = match.group(0).strip()
         
         newFile.append(row)
 
     return newFile
+
+
+def removeInvalids(file):
+    if file[0]['Media Type'] == VoiceName:
+        return removeInvalidPhoneNumbers(file)
+    elif file[0]['Media Type'] == EmailName:
+        return removeInvalidEmail(file)
 
 def removeInvalidPhoneNumbers(file):
     newFile = []
@@ -57,13 +71,40 @@ def removeInvalidPhoneNumbers(file):
     #| Note: this does NOT match +316 mobile numbers, or foreign mobile numbers
 
     for row in file:
-        phoneLine = row['Remote']
+        phoneLine = row[PhoneNumberColumn]
         match = re.search(r'^6\d{8}\s*', phoneLine)
         if match:
-            row['Remote'] = "0" + match.group(0).strip()
+            row[PhoneNumberColumn] = "0" + match.group(0).strip()
             newFile.append(row)
     
     return newFile
+
+def removeInvalidEmail(file):
+    newFile = []
+
+    for row in file:
+        emailLine = row[EmailColumn]
+        emailLine = emailLine.split(";")[-1].strip()
+
+        if '@dunea' in emailLine.lower():
+            continue
+
+        if 'noreply' in emailLine.lower() or 'no-reply' in emailLine.lower():
+            continue
+
+        newRow = row
+        newRow[EmailColumn] = emailLine
+
+        newFile.append(row)
+
+    return newFile
+
+def removeDuplicates(file):
+    if file[0]['Media Type'] == VoiceName:
+        return removeDuplicatePhoneNumbers(file)
+    elif file[0]['Media Type'] == EmailName:
+        return removeDuplicateEmails(file)
+
 
 def removeDuplicatePhoneNumbers(file):
     newFile = []
@@ -71,7 +112,7 @@ def removeDuplicatePhoneNumbers(file):
     for oldRow in file:
         addThisRow = True
         for newRow in newFile:
-            if oldRow['Remote'] == newRow['Remote']:
+            if oldRow[PhoneNumberColumn] == newRow[PhoneNumberColumn]:
                 addThisRow = False
                 break
         
@@ -79,6 +120,22 @@ def removeDuplicatePhoneNumbers(file):
             newFile.append(oldRow)
 
     return newFile
+
+def removeDuplicateEmails(file):
+    newFile = []
+
+    for oldRow in file:
+        addThisRow = True
+        for newRow in newFile:
+            if oldRow[EmailColumn] == newRow[EmailColumn]:
+                addThisRow = False
+                break;
+
+        if addThisRow:
+            newFile.append(oldRow)
+
+    return newFile
+
 
 def writeCSV(fileToWrite, newFileName):
     if os.path.exists(newFileName):
@@ -93,6 +150,7 @@ def writeCSV(fileToWrite, newFileName):
 
         writer.writeheader()
         writer.writerows(fileToWrite)
+
 
 def main():
     newFile = []
@@ -113,15 +171,14 @@ def main():
         elif fileToLoad[-4:] != '.csv':
             print('Het ingevoerde bestand is geen CSV bestand.')
 
-
     fileToWrite = os.path.join(os.path.dirname(fileToLoad), "output.csv")
 
     with open(fileToLoad, newline='') as csvfile:
         reader = csv.DictReader(csvfile)
 
         reader = removeMultipleNames(reader)
-        reader = removeInvalidPhoneNumbers(reader)
-        reader = removeDuplicatePhoneNumbers(reader)
+        reader = removeInvalids(reader)
+        reader = removeDuplicates(reader)
 
         for row in reader:
             try:
@@ -137,7 +194,7 @@ def main():
 
     writeCSV(newFile, fileToWrite)
     print('De conversie is voltooid! Het resultaat staat op de volgende locatie: ' + fileToWrite)
-    
+
 
 if __name__ == '__main__':
     main()
